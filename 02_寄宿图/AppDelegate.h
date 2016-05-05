fc2d36b5310025e5390094ba2e95b5314f2f2ce3
Yuen-rMBP <rxg9527@sina.cn>
2016-05-05 23:34:23 +0800

    0505 layer添加图片

---
+++ 02_寄宿图/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  02_寄宿图
+//
+//  Created by Yuen on 16/5/5.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 02_寄宿图/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  02_寄宿图
+//
+//  Created by Yuen on 16/5/5.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
