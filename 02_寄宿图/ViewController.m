380470866f4c118be15ef476a9b8cdbcb7852df5
Yuen-rMBP <rxg9527@sina.cn>
2016-05-06 00:13:45 +0800

    0506 contentsRect不是按点来计算的，它使用了单位坐标，单位坐标指定在0到1之间，是一个相对值（像素和点就是绝对值）。所以他们是相对与寄宿图的尺寸的。

--- 02_寄宿图/ViewController.m
+++ 02_寄宿图/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (nonatomic, weak) IBOutlet UIView *coneView;
+@property (nonatomic, weak) IBOutlet UIView *shipView;
+@property (nonatomic, weak) IBOutlet UIView *iglooView;
+@property (nonatomic, weak) IBOutlet UIView *anchorView;
+
 @end
 
 @implementation ViewController
@@ -18,11 +23,13 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    UIImage *snowManImage = [UIImage imageNamed:@"Snowman"];
-    self.view.layer.contents = (__bridge id _Nullable)(snowManImage.CGImage);
+//    UIImage *snowManImage = [UIImage imageNamed:@"Snowman"];
+//    self.view.layer.contents = (__bridge id _Nullable)(snowManImage.CGImage);
 //    self.view.layer.contentsGravity = kCAGravityResizeAspect;
     
-    [self setLayerContentClarity];
+//    [self setLayerContentClarity];
+    
+    [self setLayerSprites];
 }
 
 /**
@@ -33,4 +40,23 @@
     self.view.layer.contentsScale = [UIScreen mainScreen].scale;
 }
 
+/**
+ *  “contentsRect不是按点来计算的，它使用了单位坐标，单位坐标指定在0到1之间，是一个相对值（像素和点就是绝对值）。所以他们是相对与寄宿图的尺寸的。”
+ */
+- (void)addSpriteImage:(UIImage *)image
+       withContentRect:(CGRect)rect
+               toLayer:(CALayer *)layer {
+    layer.contentsGravity = kCAGravityResizeAspect;
+    layer.contents = (__bridge id)image.CGImage;
+    layer.contentsRect = rect;
+}
+
+- (void)setLayerSprites {
+    UIImage *image = [UIImage imageNamed:@"Sprites"];
+    
+    [self addSpriteImage:image withContentRect:CGRectMake(0, 0, 0.5, 0.5) toLayer:self.coneView.layer];
+    [self addSpriteImage:image withContentRect:CGRectMake(0.5, 0, 0.5, 0.5) toLayer:self.shipView.layer];
+    [self addSpriteImage:image withContentRect:CGRectMake(0, 0.5, 0.5, 0.5) toLayer:self.iglooView.layer];
+    [self addSpriteImage:image withContentRect:CGRectMake(0.5, 0.5, 0.5, 0.5) toLayer:self.anchorView.layer];
+}
 @end
