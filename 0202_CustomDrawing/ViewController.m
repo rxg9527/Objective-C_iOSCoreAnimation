e40c6d513a17398b00aa64f15817bc4af15c924d
Yuen-rMBP <rxg9527@sina.cn>
2016-08-18 16:56:21 +0800

    custom drawing

--- 0202_CustomDrawing/ViewController.m
+++ 0202_CustomDrawing/ViewController.m
@@ -1,14 +1,16 @@
 //
 //  ViewController.m
-//  0202_CustomDrawing
+//  01_图层树
 //
-//  Created by Yuen on 16/9/13.
+//  Created by Yuen on 16/4/30.
 //  Copyright © 2016年 Yuen. All rights reserved.
 //
 
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIView *layerView;
 
 @end
 
@@ -17,11 +19,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    CALayer *blueLayer = [CALayer new];
+    blueLayer.backgroundColor = [UIColor blueColor].CGColor;
+    blueLayer.frame = CGRectMake(50, 50, 100, 100);
+    [self.layerView.layer addSublayer:blueLayer];
+    
+    //set controller as layer delegate
+    blueLayer.delegate = self;
+    
+    //ensure that layer backing image uses correct scale
+    blueLayer.contentsScale = [UIScreen mainScreen].scale; //add layer to our view
+
+    //force layer to redraw
+    /**
+     *  我们在blueLayer上显式地调用了-display。不同于UIView，当图层显示在屏幕上时，CALayer不会自动重绘它的内容。它把重绘的决定权交给了开发者。
+     */
+    [blueLayer display];
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+/**
+ *  CALayerDelegate
+ *  除非你创建了一个单独的图层，你几乎没有机会用到CALayerDelegate协议
+ *  因为当UIView创建了它的宿主图层时，它就会自动地把图层的delegate设置为它自己，并提供了一个-displayLayer:的实现
+ *  当使用寄宿了视图的图层的时候，你也不必实现-displayLayer:和-drawLayer:inContext:方法来绘制你的寄宿图。通常做法是实现UIView的-drawRect:方法，UIView就会帮你做完剩下的工作，包括在需要重绘的时候调用-display方法。
+ 
+ *  尽管我们没有用masksToBounds属性，绘制的那个圆仍然沿边界被裁剪了。这是因为当你使用CALayerDelegate绘制寄宿图的时候，并没有对超出边界外的内容提供绘制支持。
+ */
+- (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)ctx {
+    //draw a thick red circle
+    CGContextSetLineWidth(ctx, 10.f);
+    CGContextSetStrokeColorWithColor(ctx, [UIColor redColor].CGColor);
+    CGContextStrokeEllipseInRect(ctx, layer.bounds);
 }
 
 @end
