9d5670556ad70f131c7ce930a6336f834dcfb799
Yuen-rMBP <rxg9527@sina.cn>
2016-08-19 10:17:59 +0800

    初始化

---
+++ 0304_触摸+-containPoint:/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0304_触摸+-containPoint:
+//
+//  Created by Yuen on 16/8/19.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0304_触摸+-containPoint:/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0304_触摸+-containPoint:
+//
+//  Created by Yuen on 16/8/19.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
