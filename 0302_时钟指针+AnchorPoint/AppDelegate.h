3f4aab2fc0f62f0e0b4d254e0f60c9b7241c31f5
Yuen-rMBP <rxg9527@sina.cn>
2016-08-19 09:22:21 +0800

    初始化

---
+++ 0302_时钟指针+AnchorPoint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0302_时钟指针+AnchorPoint
+//
+//  Created by Yuen on 16/8/19.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0302_时钟指针+AnchorPoint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0302_时钟指针+AnchorPoint
+//
+//  Created by Yuen on 16/8/19.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
