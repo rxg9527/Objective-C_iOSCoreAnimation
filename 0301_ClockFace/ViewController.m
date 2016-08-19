20a353aa4c0bb5e4286aa61980e4a7bd805b7aba
Yuen-rMBP <rxg9527@sina.cn>
2016-08-19 09:16:20 +0800

    时钟动画

--- 0301_ClockFace/ViewController.m
+++ 0301_ClockFace/ViewController.m
@@ -14,6 +14,8 @@
 @property (nonatomic, weak) IBOutlet UIImageView *minuteHand;
 @property (nonatomic, weak) IBOutlet UIImageView *secondHand;
 
+@property (nonatomic, strong) NSTimer *timer;
+
 @end
 
 @implementation ViewController
@@ -21,11 +23,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //开始定时器
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.f
+                                                  target:self
+                                                selector:@selector(tick)
+                                                userInfo:nil
+                                                 repeats:YES];
+    //设置刚启动时 针的正确位置
+    [self tick];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)tick
+{
+    // 取得当前时间的时分秒
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierChinese];
+    NSCalendarUnit units = NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
+    NSDateComponents *components = [calendar components:units fromDate:[NSDate date]];
+    
+    // 计算各针的角度
+    CGFloat hourAngle = (components.hour / 12.f) * M_PI * 2.0;
+    CGFloat minuteAngle = (components.minute / 60.f) * M_PI * 2.0;
+    CGFloat secondAngle = (components.second / 60.f) * M_PI * 2.0;
+    
+    // 旋转针的方向
+    self.hourHand.transform = CGAffineTransformMakeRotation(hourAngle);
+    self.minuteHand.transform = CGAffineTransformMakeRotation(minuteAngle);
+    self.secondHand.transform = CGAffineTransformMakeRotation(secondAngle);
 }
 
 @end
