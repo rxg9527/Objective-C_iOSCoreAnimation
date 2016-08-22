ea212ed93051671a46d748fd52ad71a45e273da3
Yuen-rMBP <rxg9527@sina.cn>
2016-08-22 19:16:00 +0800

    两个组合view

---
+++ 0401_圆角+masksToBounds/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0401_圆角+masksToBounds
+//
+//  Created by Yuen on 16/8/22.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0401_圆角+masksToBounds/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0401_圆角+masksToBounds
+//
+//  Created by Yuen on 16/8/22.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
