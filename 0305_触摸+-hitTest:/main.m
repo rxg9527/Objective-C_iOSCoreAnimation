28177fab93bbf01f2aa3d561a27627c9c81455c0
Yuen-rMBP <rxg9527@sina.cn>
2016-08-22 10:20:54 +0800

    初始化

---
+++ 0305_触摸+-hitTest:/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  0305_触摸+-hitTest:
+//
+//  Created by Yuen on 16/8/22.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
