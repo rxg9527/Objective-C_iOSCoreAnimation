572ce4341d33b7a9ae344ab8d028e92d04d3eda0
Yuen-rMBP <rxg9527@sina.cn>
2016-08-23 02:21:44 +0800

    初始化

---
+++ 0405_图层蒙版+mask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0405_图层蒙版+mask
+//
+//  Created by Yuen on 16/8/23.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0405_图层蒙版+mask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0405_图层蒙版+mask
+//
+//  Created by Yuen on 16/8/23.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
