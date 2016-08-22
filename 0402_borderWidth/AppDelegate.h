4d881c97b17989f8c307892f47e05ab43e66c59e
Yuen-rMBP <rxg9527@sina.cn>
2016-08-22 19:41:09 +0800

    初始化

---
+++ 0402_borderWidth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0402_borderWidth
+//
+//  Created by Yuen on 16/8/22.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0402_borderWidth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0402_borderWidth
+//
+//  Created by Yuen on 16/8/22.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
