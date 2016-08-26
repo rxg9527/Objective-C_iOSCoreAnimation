220c59a20ea2bac4c86e95e3711c7ff6ef400d30
Yuen-rMBP <rxg9527@sina.cn>
2016-08-26 17:37:39 +0800

    组透明 iOS7以后默认开启UIViewGroupOpacity

---
+++ 0407_组透明+shouldRasterize/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0407_组透明+shouldRasterize
+//
+//  Created by Yuen on 16/8/26.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0407_组透明+shouldRasterize/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0407_组透明+shouldRasterize
+//
+//  Created by Yuen on 16/8/26.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
