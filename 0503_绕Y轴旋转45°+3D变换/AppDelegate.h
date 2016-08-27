cc80a839c2ab77503a74d8acb92348608bb037db
Yuen-rMBP <rxg9527@sina.cn>
2016-08-27 13:11:52 +0800

    初始化

---
+++ 0503_绕Y轴旋转45°+3D变换/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0503_绕Y轴旋转45°+3D变换
+//
+//  Created by Yuen on 16/8/27.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0503_绕Y轴旋转45°+3D变换/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  0503_绕Y轴旋转45°+3D变换
+//
+//  Created by Yuen on 16/8/27.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
