f4e78c61977e39ab59f755caaa686a90683abfab
Yuen-rMBP <rxg9527@sina.cn>
2016-08-28 10:53:47 +0800

    初始化

---
+++ 0507_绕Y轴相反的旋转变换+扁平化图层/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0507_绕Y轴相反的旋转变换+扁平化图层
+//
+//  Created by Yuen on 16/8/28.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0507_绕Y轴相反的旋转变换+扁平化图层/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  0507_绕Y轴相反的旋转变换+扁平化图层
+//
+//  Created by Yuen on 16/8/28.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
