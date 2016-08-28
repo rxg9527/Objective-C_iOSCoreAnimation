e1d77a78e17826d02379f3105542009e8168e962
Yuen-rMBP <rxg9527@sina.cn>
2016-08-28 10:59:55 +0800

    绕Y轴相反的旋转变换 原因和结果

--- 0507_绕Y轴相反的旋转变换+扁平化图层/ViewController.m
+++ 0507_绕Y轴相反的旋转变换+扁平化图层/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (nonatomic, weak) IBOutlet UIView *outerView;
+@property (nonatomic, weak) IBOutlet UIView *innerView;
+
 @end
 
 @implementation ViewController
@@ -17,11 +20,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    //rotate the outer layer 45 degrees
+    CATransform3D outer = CATransform3DIdentity;
+    outer.m34 = -1.0 / 500.0;
+    outer = CATransform3DRotate(outer, M_PI_4, 0, 1, 0);
+    self.outerView.layer.transform = outer;
+    
+    //rotate the inner layer -45 degrees
+    CATransform3D inner = CATransform3DIdentity;
+    inner.m34 = -1.0 / 500.0;
+    inner = CATransform3DRotate(inner, -M_PI_4, 0, 1, 0);
+    self.innerView.layer.transform = inner;
 }
 
 @end
