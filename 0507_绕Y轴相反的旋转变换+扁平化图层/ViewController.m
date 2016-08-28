29abaa481a99468cd1f15e2b93985422ad3372ac
Yuen-rMBP <rxg9527@sina.cn>
2016-08-28 11:00:53 +0800

    修改注释

--- 0507_绕Y轴相反的旋转变换+扁平化图层/ViewController.m
+++ 0507_绕Y轴相反的旋转变换+扁平化图层/ViewController.m
@@ -21,6 +21,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    /**
+     *  尽管Core Animation图层存在于3D空间之内，但它们并不都存在同一个3D空间。每个图层的3D场景其实是扁平化的，当你从正面观察一个图层，看到的实际上由子图层创建的想象出来的3D场景，但当你倾斜这个图层，你会发现实际上这个3D场景仅仅是被绘制在图层的表面。
+     */
     //rotate the outer layer 45 degrees
     CATransform3D outer = CATransform3DIdentity;
     outer.m34 = -1.0 / 500.0;
