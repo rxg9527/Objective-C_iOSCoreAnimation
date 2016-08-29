04838f30faa04cf8f01074986b50d0e73a48fe54
Yuen-rMBP <rxg9527@sina.cn>
2016-08-29 13:52:12 +0800

    基于CATextLayer的Label+CATextLayer作为宿主图层

---
+++ 0604_基于CATextLayer的Label+CATextLayer作为宿主图层/XGLayerLabel.h
@@ -0,0 +1,13 @@
+//
+//  XGLayerLabel.h
+//  Objective-C_iOSCoreAnimation
+//
+//  Created by Yuen on 16/8/29.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XGLayerLabel : UILabel
+
+@end
