1da261f99ecba85dbbe29f4445226e58873a2bcd
Yuen-rMBP <rxg9527@sina.cn>
2016-08-29 12:45:11 +0800

    CATextLayer+NSAttributedString

--- 0603_CATextLayer+NSAttributedString/ViewController.m
+++ 0603_CATextLayer+NSAttributedString/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <CoreText/CoreText.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,58 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //create a text layer
+    CATextLayer *textLayer = [CATextLayer layer];
+    textLayer.frame = self.view.bounds;
+    textLayer.contentsScale = [UIScreen mainScreen].scale;
+    [self.view.layer addSublayer:textLayer];
+    
+    //set text attributes
+    textLayer.alignmentMode = kCAAlignmentJustified;
+    textLayer.wrapped = YES;
+    
+    //choose a font
+    UIFont *font = [UIFont systemFontOfSize:15];
+    
+    //choose some text
+    NSString *text = @"Lorem ipsum dolor sit amet, consectetur adipiscing \
+    elit. Quisque massa arcu, eleifend vel varius in, facilisis pulvinar \
+    leo. Nunc quis nunc at mauris pharetra condimentum ut ac neque. Nunc \
+    elementum, libero ut porttitor dictum, diam odio congue lacus, vel \
+    fringilla sapien diam at purus. Etiam suscipit pretium nunc sit amet \
+    lobortis";
+    
+    //create attributed string
+    NSMutableAttributedString *string = nil;
+    string = [[NSMutableAttributedString alloc] initWithString:text];
+    
+    //convert UIFont to a CTFont
+    CFStringRef fontName = (__bridge CFStringRef)font.fontName;
+    CGFloat fontSize = font.pointSize;
+    CTFontRef fontRef = CTFontCreateWithName(fontName, fontSize, NULL);
+    
+    //set text attributes
+    NSDictionary *attribs = @{
+                              (__bridge id)kCTForegroundColorAttributeName:
+                                  (__bridge id)[UIColor blackColor].CGColor,
+                              (__bridge id)kCTFontAttributeName: (__bridge id)fontRef
+                              };
+    [string setAttributes:attribs range:NSMakeRange(0, [text length])];
+    attribs = @{
+                (__bridge id)kCTForegroundColorAttributeName:
+                    (__bridge id)[UIColor redColor].CGColor,
+                (__bridge id)kCTUnderlineStyleAttributeName:
+                    @(kCTUnderlineStyleSingle),
+                (__bridge id)kCTFontAttributeName: (__bridge id)fontRef
+                };
+    [string setAttributes:attribs range:NSMakeRange(6, 5)];
+    
+    //release the CTFont we created earlier
+    CFRelease(fontRef);
+    
+    //set layer text
+    textLayer.string = string;
 }
 
 @end
