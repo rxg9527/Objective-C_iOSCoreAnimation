0619f751cc04d261014d33f79034b31febad04f3
Yuen-rMBP <rxg9527@sina.cn>
2016-08-29 12:42:05 +0800

    初始化

---
+++ 0603_CATextLayer+NSAttributedString/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0603_CATextLayer+NSAttributedString
+//
+//  Created by Yuen on 16/8/29.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 0603_CATextLayer+NSAttributedString/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  0603_CATextLayer+NSAttributedString
+//
+//  Created by Yuen on 16/8/29.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
