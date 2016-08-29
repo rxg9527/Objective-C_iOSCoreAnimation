20ea232fab853e4e78c268628d28e7a05929c5ab
Yuen-rMBP <rxg9527@sina.cn>
2016-08-29 12:03:52 +0800

    初始化

---
+++ 0602_CATextLayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0602_CATextLayer
+//
+//  Created by Yuen on 16/8/29.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 0602_CATextLayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  0602_CATextLayer
+//
+//  Created by Yuen on 16/8/29.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
