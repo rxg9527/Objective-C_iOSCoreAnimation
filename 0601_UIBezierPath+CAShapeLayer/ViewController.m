8910a2cc3c4fbfb9a24680173694f36e76ede880
Yuen-rMBP <rxg9527@sina.cn>
2016-08-29 11:40:00 +0800

    CAShapeLayer

--- 0601_UIBezierPath+CAShapeLayer/ViewController.m
+++ 0601_UIBezierPath+CAShapeLayer/ViewController.m
@@ -21,7 +21,7 @@
     //创建路径
     UIBezierPath *path = [[UIBezierPath alloc] init];
     [path moveToPoint:CGPointMake(175, 100)];
-    path addArcWithCenter:CGPointMake(150, 100) radius:25 startAngle:0 endAngle:2*M_PI clockwise:YES];
+    [path addArcWithCenter:CGPointMake(150, 100) radius:25 startAngle:0 endAngle:2*M_PI clockwise:YES];
     [path moveToPoint:CGPointMake(150, 125)];
     [path addLineToPoint:CGPointMake(150, 175)];
     [path addLineToPoint:CGPointMake(125, 225)];
@@ -30,7 +30,17 @@
     [path moveToPoint:CGPointMake(100, 150)];
     [path addLineToPoint:CGPointMake(200, 150)];
     
+    //创建 Shape Layer
+    CAShapeLayer *layer = [CAShapeLayer layer];
+    layer.strokeColor = [UIColor redColor].CGColor;
+    layer.fillColor = [UIColor clearColor].CGColor;
+    layer.lineWidth = 5;
+    layer.lineCap = kCALineCapRound;
+    layer.lineJoin = kCALineJoinRound;
+    layer.path = path.CGPath;
     
+    //添加到 view 的 layer 上
+    [self.view.layer addSublayer:layer];
 }
 
 @end
