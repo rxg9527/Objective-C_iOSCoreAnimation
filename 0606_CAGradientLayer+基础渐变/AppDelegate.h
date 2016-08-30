9b08eb16547716ba31ce76632bd266a97500b63e
Yuen-rMBP <rxg9527@sina.cn>
2016-08-30 16:47:28 +0800

    初始化

---
+++ 0606_CAGradientLayer+基础渐变/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0606_CAGradientLayer+基础渐变
+//
+//  Created by Yuen on 16/8/30.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0606_CAGradientLayer+基础渐变/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0606_CAGradientLayer+基础渐变
+//
+//  Created by Yuen on 16/8/30.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
