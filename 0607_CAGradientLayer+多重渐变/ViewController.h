6c379ddcf576d435eca8f3dea80a22a82ac0dd65
Yuen-rMBP <rxg9527@sina.cn>
2016-09-20 23:16:17 +0800

    初始化

---
+++ 0607_CAGradientLayer+多重渐变/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0607_CAGradientLayer+多重渐变
+//
+//  Created by Yuen on 16/9/20.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 0607_CAGradientLayer+多重渐变/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  0607_CAGradientLayer+多重渐变
+//
+//  Created by Yuen on 16/9/20.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
