72f02f427d5447dfac6fe91e6545898e1ca9c9e7
Yuen-rMBP <rxg9527@sina.cn>
2016-09-21 01:03:24 +0800

    ReflectionView存在问题

---
+++ 0609_CAReplicatorLayer+反射/ReflectionView.h
@@ -0,0 +1,13 @@
+//
+//  ReflectionView.h
+//  Objective-C_iOSCoreAnimation
+//
+//  Created by Yuen on 16/9/21.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReflectionView : UIView
+
+@end
