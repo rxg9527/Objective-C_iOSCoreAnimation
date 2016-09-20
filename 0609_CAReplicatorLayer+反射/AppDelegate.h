25a2afbe0a0e6d0e94780fd9df7a8ea78824c9e0
Yuen-rMBP <rxg9527@sina.cn>
2016-09-20 23:58:41 +0800

    初始化

---
+++ 0609_CAReplicatorLayer+反射/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0609_CAReplicatorLayer+反射
+//
+//  Created by Yuen on 16/9/20.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0609_CAReplicatorLayer+反射/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0609_CAReplicatorLayer+反射
+//
+//  Created by Yuen on 16/9/20.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
