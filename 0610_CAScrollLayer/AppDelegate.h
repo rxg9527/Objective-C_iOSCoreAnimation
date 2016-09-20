5cf6018a285f329eb082fef2570664f41d178fd5
Yuen-rMBP <rxg9527@sina.cn>
2016-09-21 01:25:46 +0800

    初始化

---
+++ 0610_CAScrollLayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0610_CAScrollLayer
+//
+//  Created by Yuen on 16/9/21.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0610_CAScrollLayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0610_CAScrollLayer
+//
+//  Created by Yuen on 16/9/21.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
