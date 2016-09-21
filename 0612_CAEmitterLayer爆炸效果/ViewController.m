d141ced3bd179eaf81e00658dc0673bd9de970b8
Yuen-rMBP <rxg9527@sina.cn>
2016-09-21 20:36:11 +0800

    CAEMitterCell的属性

--- 0612_CAEmitterLayer爆炸效果/ViewController.m
+++ 0612_CAEmitterLayer爆炸效果/ViewController.m
@@ -28,6 +28,12 @@
     emitter.emitterPosition = CGPointMake(emitter.frame.size.width / 2.0, emitter.frame.size.height / 2.0);
     
     //create a particle template
+    /**
+     CAEMitterCell的属性基本上可以分为三种：
+     这种粒子的某一属性的初始值。比如，color属性指定了一个可以混合图片内容颜色的混合色。在示例中，我们将它设置为桔色。
+     例子某一属性的变化范围。比如emissionRange属性的值是2π，这意味着例子可以从360度任意位置反射出来。如果指定一个小一些的值，就可以创造出一个圆锥形
+     指定值在时间线上的变化。比如，在示例中，我们将alphaSpeed设置为-0.4，就是说例子的透明度每过一秒就是减少0.4，这样就有发射出去之后逐渐小时的效果。
+     */
     CAEmitterCell *cell = [[CAEmitterCell alloc] init];
     cell.contents = (__bridge id)[UIImage imageNamed:@"Spark.png"].CGImage;
     cell.birthRate = 150;
