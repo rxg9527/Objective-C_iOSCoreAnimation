d1a8e2888c350fa1cbfb7731f02a6da285293a43
Yuen-rMBP <rxg9527@sina.cn>
2016-09-21 20:34:30 +0800

    CAEmitterLayer爆炸效果

--- 0612_CAEmitterLayer爆炸效果/ViewController.m
+++ 0612_CAEmitterLayer爆炸效果/ViewController.m
@@ -17,6 +17,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //create particle emitter layer
+    CAEmitterLayer *emitter = [CAEmitterLayer layer];
+    emitter.frame = self.view.bounds;
+    [self.view.layer addSublayer:emitter];
+    
+    //configure emitter
+    emitter.renderMode = kCAEmitterLayerAdditive;
+    emitter.emitterPosition = CGPointMake(emitter.frame.size.width / 2.0, emitter.frame.size.height / 2.0);
+    
+    //create a particle template
+    CAEmitterCell *cell = [[CAEmitterCell alloc] init];
+    cell.contents = (__bridge id)[UIImage imageNamed:@"Spark.png"].CGImage;
+    cell.birthRate = 150;
+    cell.lifetime = 5.0;
+    cell.color = [UIColor colorWithRed:1 green:0.5 blue:0.1 alpha:1.0].CGColor;
+    cell.alphaSpeed = -0.4;
+    cell.velocity = 50;
+    cell.velocityRange = 50;
+    cell.emissionRange = M_PI * 2.0;
+    
+    //add particle template to emitter
+    emitter.emitterCells = @[cell];
 }
 
 @end
