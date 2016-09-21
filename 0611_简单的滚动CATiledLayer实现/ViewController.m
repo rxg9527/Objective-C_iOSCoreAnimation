fa354e21691eb74eff97230e578ded96ababf92f
Yuen-rMBP <rxg9527@sina.cn>
2016-09-21 17:05:27 +0800

    简单的滚动CATiledLayer实现

--- 0611_简单的滚动CATiledLayer实现/ViewController.m
+++ 0611_简单的滚动CATiledLayer实现/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <CALayerDelegate>
+
+@property (nonatomic, weak) IBOutlet UIScrollView *scrollView;
 
 @end
 
@@ -16,14 +18,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //add the tiled layer
+    CATiledLayer *tileLayer = [CATiledLayer layer];
+    tileLayer.frame = CGRectMake(0, 0, 2048, 2048);
+    tileLayer.delegate = self;
+    [self.scrollView.layer addSublayer:tileLayer];
+    
+    //configure the scroll view
+    self.scrollView.contentSize = tileLayer.frame.size;
+    
+    //draw layer
+    [tileLayer setNeedsDisplay];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)drawLayer:(CATiledLayer *)layer inContext:(CGContextRef)ctx {
+    //determine tile coordinate
+    CGRect bounds = CGContextGetClipBoundingBox(ctx);
+    NSInteger x = floor(bounds.origin.x / layer.tileSize.width);
+    NSInteger y = floor(bounds.origin.y / layer.tileSize.height);
+    
+    //load tile image
+    NSString *imageName = [NSString stringWithFormat:@"Snowman_%02li_%02li", (long)x, (long)y];
+    NSString *imagePath = [[NSBundle mainBundle] pathForResource:imageName ofType:@"jpg"];
+    UIImage *tileImage = [UIImage imageWithContentsOfFile:imagePath];
+    
+    //draw tile
+    UIGraphicsPushContext(ctx);
+    [tileImage drawInRect:bounds];
+    UIGraphicsPopContext();
 }
-
 
 @end
