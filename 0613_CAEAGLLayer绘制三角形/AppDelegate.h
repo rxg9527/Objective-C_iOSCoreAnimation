840c8354f97d73fb1101b4e8dded52258514553c
Yuen-rMBP <rxg9527@sina.cn>
2016-09-22 22:18:28 +0800

    初始化

---
+++ 0613_CAEAGLLayer绘制三角形/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0613_CAEAGLLayer绘制三角形
+//
+//  Created by Yuen on 16/9/22.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0613_CAEAGLLayer绘制三角形/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0613_CAEAGLLayer绘制三角形
+//
+//  Created by Yuen on 16/9/22.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
