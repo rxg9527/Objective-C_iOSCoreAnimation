0d3032987eb8d3261be95b8fedab3dc47e7d1f7d
Yuen-rMBP <rxg9527@sina.cn>
2016-09-23 16:38:22 +0800

    已完成（显示存在问题）

--- 0615_AVPlayerLayer+圆角边框变换/ViewController.m
+++ 0615_AVPlayerLayer+圆角边框变换/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
+
+@property (nonatomic, weak) IBOutlet UIView *containerView;
 
 @end
 
@@ -17,6 +20,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //get video URL
+    NSURL *URL = [[NSBundle mainBundle] URLForResource:@"Ship"
+                                         withExtension:@"mp4"];
+    
+    //create player and player layer
+    AVPlayer *player = [AVPlayer playerWithURL:URL];
+    AVPlayerLayer *playerLayer = [AVPlayerLayer playerLayerWithPlayer:player];
+    
+    //set player layer frame and attach it to our view
+    playerLayer.frame = self.containerView.bounds;
+    [self.containerView.layer addSublayer:playerLayer];
+    
+    //transform layer
+    CATransform3D transform = CATransform3DIdentity;
+    transform.m34 = -1.0 / 500.0;
+    transform = CATransform3DRotate(transform, M_PI_4, 1, 1, 0);
+    playerLayer.transform = transform;
+    
+    //add rounded corners and border
+    playerLayer.masksToBounds = YES;
+    playerLayer.cornerRadius = 20.0;
+    playerLayer.borderColor = [UIColor redColor].CGColor;
+    playerLayer.borderWidth = 5.0;
+    
+    //play the video
+    [player play];
+
 }
 
 
