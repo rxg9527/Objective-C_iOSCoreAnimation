ee242a2774116ab0edd20511bb9c6e83f680ec2f
Yuen-rMBP <rxg9527@sina.cn>
2016-09-23 15:31:21 +0800

    初始化

---
+++ 0614_AVPlayerLayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0614_AVPlayerLayer
+//
+//  Created by Yuen on 16/9/23.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
