740770ea4d214ddadd6c7cd83e75267f5acaaaf9
Yuen-rMBP <rxg9527@sina.cn>
2016-09-23 16:35:20 +0800

    初始化

---
+++ 0615_AVPlayerLayer+圆角边框变换/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0615_AVPlayerLayer+圆角边框变换
+//
+//  Created by Yuen on 16/9/23.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0615_AVPlayerLayer+圆角边框变换/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0615_AVPlayerLayer+圆角边框变换
+//
+//  Created by Yuen on 16/9/23.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
